d4f254d6812703928acbd125f49deab0acd55bee
Zihad-hasan-maruf <invisiblegamer7211@gmail.com>
2024-06-19 22:33:18 +0600

    initial commit

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -24,7 +24,7 @@ const Comments = ({ postSlug }) => {
   const { status } = useSession();
 
   const { data, mutate, isLoading } = useSWR(
-    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    `https://web-blogz.vercel.app/api/comments?postSlug=${postSlug}`,
     fetcher
   );
 
